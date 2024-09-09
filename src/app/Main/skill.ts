7f840a66abe92d4af91b24a505f5f3860ca4b91b
Hazim Bakar <hazimbaa@gmail.com>
2024-09-09 21:13:57 +0800

    about and skill draft

---
+++ src/app/Main/skill.ts
@@ -0,0 +1,40 @@
+
+
+export const skills = [
+    {
+        name: 'Javascript',
+        logo: ''
+    },
+    {
+        name: 'React JS',
+        logo: ''
+    },
+    {
+        name: 'Tailwind CSS',
+        logo: ''
+    },
+    {
+        name: 'TypeScript',
+        logo: ''
+    },
+    {
+        name: 'Antd',
+        logo: ''
+    },
+    {
+        name: 'Figma',
+        logo: ''
+    },
+    {
+        name: 'MongoDB',
+        logo: ''
+    },
+    {
+        name: 'Git Source Control',
+        logo: ''
+    },
+    {
+        name: 'MySQL',
+        logo: ''
+    }
+]
