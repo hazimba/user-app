3b85723f7c6a56db3b9fc5b8bbf4a9ffdc2fc963
Hazim Bakar <hazimbaa@gmail.com>
2024-09-11 21:54:10 +0800

    change font, and image

--- src/app/Main/Introduction.tsx
+++ src/app/Main/Introduction.tsx
@@ -1,20 +1,21 @@
-import { Button, Image } from "antd"
-
+import { Button } from "antd"
+ 
+import Image from "next/image"
 
 const Introduction = () => {
 
     return (
-    <div className="flex w-screen justify-evenly h-[550px] items-center">
+    <div className="flex w-screen justify-evenly h-[450px] items-center">
         <div className="">
-          <div>IM HAZIM BAKAR</div>
+          <div>IM <span className="font-sans">HAZIM BAKAR</span></div>
           <br />
           <div>WEB && SOFTWARE</div>
           <div>DEVELOPER</div>
           <br />
           <Button>Contact</Button>
         </div>
-        <div>
-          <Image src="https://media.licdn.com/dms/image/v2/D5603AQFPc3H1g7MAzA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1721125764819?e=1731542400&v=beta&t=QpgBiq0l4hmQcX7XBIBkWX2aoU3TqHVlp_ZZO9vsPpk" width={200} alt="" />
+        <div className="">
+          <Image className="rounded-full" width={350} height={2000} src="/hazim2.png" alt="" />
         </div>
       </div>
     )
