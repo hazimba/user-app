478837ff7ef4279a33a193d168b633eb008c92fb
hazim <hazim.abubakar@swiftlogistics.com.my>
2024-09-13 21:27:36 +0800

    added logo, antd package

--- src/app/Main/Header.tsx
+++ src/app/Main/Header.tsx
@@ -1,14 +1,54 @@
-
+import { UserOutlined } from "@ant-design/icons"
+import Image from "next/image"
+import { Button, Dropdown, Space } from 'antd';
+import type { MenuProps } from 'antd';
 
 
 const Header = () => {
 
+  const items: MenuProps['items'] = [
+    {
+      key: '1',
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/hazimba">
+          GitHub Profile
+        </a>
+      ),
+    },
+    {
+      key: '2',
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/hazimba/">
+          LinkendIn
+        </a>
+      ),
+    },
+    {
+      key: '3',
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/hazimba/">
+          Instagram
+        </a>
+      ),
+    }
+  ]
+
   return (
-    <div className="flex gap-10 py-8">
-      <div>About</div>
-      <div>Portfolio</div>
-      <div>Contact</div>
+    <div className="flex w-screen items-center px-8">
+      <div className="">
+        <Image src="/logo2.png" alt="Hazim Bakar" width={50} height={50} />
+      </div>
+      <div className="flex flex-1 justify-center gap-10 py-8">
+        <div>About</div>
+        <div>Portfolio</div>
+        <div>Contact</div>
+      </div>
+      <div className="">
+        <Dropdown menu={{ items }}><UserOutlined style={{ fontSize: '20px' }} /></Dropdown>
+      </div>
     </div>
+
+
   )
 }
 export default Header
--- src/app/Main/Introduction.tsx
+++ src/app/Main/Introduction.tsx
@@ -4,15 +4,34 @@ import Image from "next/image"
 
 const Introduction = () => {
 
+  const random = () => {
+    const name = ['H', 'A', 'Z', 'I', 'M', 'B', 'A', 'K', 'A', 'R'];
+
+    return name.map((n, index) => {
+      const randomPadding = Math.floor(Math.random() * 20);
+      return (
+        <span key={index} style={{ paddingLeft: `${randomPadding}px`, paddingBottom: `${randomPadding}px` }}>
+          {n}
+        </span>
+      );
+    });
+  };
+
+
+
   return (
     <div className="flex w-screen justify-evenly h-[450px] items-center font-sans">
-      <div className="">
-        <div>IM <span className="font-sans">HAZIM BAKAR</span></div>
-        <br />
-        <div>WEB && SOFTWARE</div>
-        <div>DEVELOPER</div>
-        <br />
-        <Button>Contact</Button>
+      <div className="flex flex-col gap-5">
+        <div className="text-2xl flex items-end justify-center">IM <span className="text-5xl pl-2 pr-4">HAZIM BAKAR</span></div>
+
+        <div>
+          <div>JAVASCRIPT REACT JS</div>
+          <div>DEVELOPER</div>
+        </div>
+
+        <div className="pt-20">
+          <Button type="primary">Contact</Button>
+        </div>
       </div>
       <div className="">
         <Image className="rounded-full" width={350} height={2000} src="/hazim2.png" alt="" />
